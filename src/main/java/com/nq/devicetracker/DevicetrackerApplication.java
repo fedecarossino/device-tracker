cbb6eb518a634d651f1863267bea333be5faf309
Federico Carossino <>
2018-04-10 21:43:11 -0300

    java + springboot+ jpa + memory database

---
+++ src/main/java/com/nq/devicetracker/DevicetrackerApplication.java
@@ -0,0 +1,12 @@
+package com.nq.devicetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicetrackerApplication.class, args);
+	}
+}
